939dd404f8ce1846926d02cb5c671635d005c13d
Abno <abnomaurya98@gmail.com>
2024-01-03 21:19:42 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import ToDo from './components/ToDo.vue';
+
+export default {
+    setup() {
+        components: {
+            ToDo;
+        }
+    },
+    components: { ToDo }
+}
+</script>
+
+<template>
+  <div id="app">
+    <ToDo />
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="to-do">
+        <h1 class="text-3xl font-bold underline">
+         ToDo Application!
+       </h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .form {
+        border: 1px solid red;
+        width: 50%;
+        text-align: center;
+    }
+</style>
