63ba89102a361ea9f70819a06b60da7f71916a94
Abno <abnomaurya98@gmail.com>
2024-01-05 21:51:45 +0530

    Worked on Input field

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="to-do">
-        
+    <div class="task-input w-[60%] flex justify-center items-center">
+        <input type="text" class="p-3 border-2 border-blue-200 top-7 rounded-lg shadow-[0_2px_14px_1px_rgba(96,165,250,0.1)] w-[50%]" placeholder="Enter Task...">
     </div>
 </template>
 <script>
@@ -15,3 +15,4 @@ export default {
         text-align: center;
     }
 </style>
+<!-- 96 165 250 / 0.3 -->
