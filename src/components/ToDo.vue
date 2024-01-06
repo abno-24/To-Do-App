239bdcbb09d4cd2f36cf24401f5e73315af2def0
Abno <abhinavmaurya605@gmail.com>
2024-01-06 21:24:41 +0530

    Changes in the input filed

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="task-input w-[60%] flex justify-center items-center">
-        <input type="text" class="p-3 border-2 border-blue-200 top-7 rounded-lg shadow-[0_2px_14px_1px_rgba(96,165,250,0.1)] w-[50%]" placeholder="Enter Task...">
+        <input type="text"
+            class="p-3 border-2 border-blue-200 top-7 rounded-lg shadow-[0_2px_14px_1px_rgba(96,165,250,0.1)] w-[50%] outline-none"
+            placeholder="Enter Task...">
     </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
 <style scoped>
-    .form {
-        border: 1px solid red;
-        width: 50%;
-        text-align: center;
-    }
+.form {
+    border: 1px solid red;
+    width: 50%;
+    text-align: center;
+}
 </style>
 <!-- 96 165 250 / 0.3 -->
