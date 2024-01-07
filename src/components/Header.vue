d3ed3f90f76a540e7d5ec1dc86fbb5fa5e00bc0f
Abno <abhinavmaurya605@gmail.com>
2024-01-07 18:50:31 +0530

    Worked on responsiveness

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="header flex justify-between items-center sm:px-3 md:px-3 py-2 px-20">
+    <div
+        class="absolute flex justify-between items-center w-full h-16 top-0 py-2 px-20 shadow-[0_2px_14px_1px_rgba(0,0,0,0.03)] max-md:px-8">
         <h1 class="text-xl font-bold">To-Do</h1>
         <div class="icon">
             <a href="https://github.com/abno-24/To-Do-App" target="_blank" class="hover:fill-sky-400">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" class="hover:scale-150 transition-transform">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"
+                    class="hover:scale-150 transition-transform">
                     <path
                         d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z" />
                 </svg>
@@ -16,16 +18,5 @@ export default {
 
 }
 </script>
-<style scoped>
-    .header {
-        position: absolute;
-        top: 0;
-        width: 100vw;
-        height: 4rem;
-        /* padding: 0.3rem 5rem; */
-        box-shadow: 0 2px 14px 1px rgba(0, 0, 0, 0.03);
-    }
-    /* svg:hover {
-        fill: red;
-    } */
+<style>
 </style>
--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,24 +1,30 @@
 <template>
-    <div class="task-input w-[60%] flex justify-center items-center flex-col gap-y-6">
-        <div class="flex flex-col w-[50%] gap-y-1">
+    <div class="task-input w-[60%] flex justify-center items-center flex-col gap-y-6 max-md:w-[100%] max-lg:w-[90%]">
+        <div class="flex flex-col w-[50%] gap-y-1 max-md:w-[90%] max-lg:w-[80%]">
             <label for="task">Task Name</label>
-            <input type="text" name="task"
+            <input type="text" id="task"
                 class="p-3 border-2 border-gray-400-200 top-7 rounded-lg shadow-[0_8px_14px_1px_rgba(96,165,250,0.1)] outline-none"
                 placeholder="Enter Task...">
         </div>
-        <div class="flex justify-between w-[50%] gap-y-1">
-            <div class="flex flex-col w-[45%]">
+        <div class="flex justify-between w-[50%] gap-y-1 max-md:w-[90%] max-lg:w-[80%]">
+            <div class="flex flex-col w-[45%] max-lg:w-[48%]">
                 <label for="from-time">From</label>
-                <input type="time" name="from-time"
+                <input type="time" id="from-time"
                     class="p-3 border-2 border-gray-400-200 top-7 rounded-lg shadow-[0_8px_14px_1px_rgba(96,165,250,0.1)] outline-none"
                     placeholder="Enter Task...">
             </div>
-            <div class="flex flex-col w-[45%]">
+            <div class="flex flex-col w-[45%] max-lg:w-[48%]">
                 <label for="to-time">To</label>
-                <input type="time" name="to-time"
+                <input type="time" id="to-time"
                     class="p-3 border-2 border-gray-400-200 top-7 rounded-lg shadow-[0_8px_14px_1px_rgba(96,165,250,0.1)] outline-none"
                     placeholder="Enter Task...">
             </div>
+        </div>
+        <div class="flex flex-col w-[50%] gap-y-1 max-md:w-[90%] max-lg:w-[80%]">
+            <label for="assigned">Assigned To <span class="font-light text-gray-400">(optional)</span></label>
+            <input type="text" id="assigned"
+                class="p-3 border-2 border-gray-400-200 top-7 rounded-lg shadow-[0_8px_14px_1px_rgba(96,165,250,0.1)] outline-none"
+                placeholder="Name">
         </div>
     </div>
 </template>
@@ -34,4 +40,3 @@ export default {
     text-align: center;
 }
 </style>
-<!-- 96 165 250 / 0.3 -->
