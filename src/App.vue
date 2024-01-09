9d67d52efc329c9db1221fb34a8ac790aa1a0364
Abno <abhinavmaurya605@gmail.com>
2024-01-09 16:27:43 +0530

    Worked on Info feature

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script>
 import Header from './components/Header.vue';
 import ToDo from './components/ToDo.vue';
+import Info from './components/Info.vue';
 
 export default {
     setup() {
     },
-    components: { ToDo, Header }
+    components: { ToDo, Header, Info }
 }
 </script>
 
@@ -13,6 +14,7 @@ export default {
   <div id="app">
     <Header />
     <ToDo />
+    <Info />
   </div>
 </template>
 
