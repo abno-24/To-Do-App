e5e70b1f1f0f0310be91e95a535e07a60d49796d
Abno <abhinavmaurya605@gmail.com>
2024-01-09 17:30:20 +0530

    Worked on Info feature & responsiveness

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,12 +1,13 @@
 <template>
-    <div class="absolute top-24 right-32 border border-gray-100">
-        <span class="absolute invisible text-base bg-black hover:text-white text-gray-500 hover:visible z-10 flex justify-between items-center gap-1">
-            info
-            <svg class="z-20 visible fill-slate-500 hover:bg-white" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                <path
-                d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-            </svg>
-        </span>
+    <div class="absolute top-20 w-full h-12 flex justify-end items-center pr-20 max-md:pr-8">
+        <div class="flex gap-1 invisible hover:visible transition-all">
+            <div class="text border border-blue-400 px-3 rounded-full shadow-[0_2px_14px_1px_rgba(0,0,0,0.05)]">
+                <p class="text-sm font-light">Info</p>
+            </div>
+            <span class="material-symbols-outlined cursor-pointer hover:text-yellow-400 hover:scale-110 transition-transform visible">
+                lightbulb
+            </span>
+        </div>
     </div>
 </template>
 <script>
