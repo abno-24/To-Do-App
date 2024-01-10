f6d73e878a9a9b0039c3946a32c8065881f6f9db
Abno <abhinavmaurya605@gmail.com>
2024-01-11 00:00:51 +0530

    Info feature done

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,24 +1,39 @@
 <template>
-    <div class="absolute top-20 w-full h-12 flex justify-end items-center pr-20 max-md:pr-8">
-        <div class="flex gap-1 invisible hover:visible transition-all">
+    <div v-if="toggleImg == true"
+        class="absolute w-full h-full border bg-gray-800/[.3] flex justify-center items-center flex-col gap-4">
+        <span class="material-symbols-outlined self-end mr-4 text-white cursor-pointer text-4xl max-md:text-3xl"
+            @click="closeImg">
+            cancel
+        </span>
+        <img src="/imgs/matrix.png" alt="Image" class="w-[60%] max-md:w-[90%]">
+        <p class="text-white text-center">
+            The image above presents a framework to guide you in creating a productive to-do list for the day!
+        </p>
+    </div>
+    <div v-else class="absolute top-20 w-full h-12 flex justify-end items-center pr-20 max-md:pr-8">
+        <div class="flex gap-1 invisible hover:visible transition-all" @click="showImg">
             <div class="text border border-blue-400 px-3 rounded-full shadow-[0_2px_14px_1px_rgba(0,0,0,0.05)]">
                 <p class="text-sm font-light">Info</p>
             </div>
-            <span class="material-symbols-outlined cursor-pointer hover:text-yellow-400 hover:scale-110 transition-transform visible">
+            <span
+                class="material-symbols-outlined cursor-pointer hover:text-yellow-400 hover:scale-110 transition-transform visible">
                 lightbulb
             </span>
         </div>
     </div>
 </template>
-<script>
-export default {
-    // setup() {
-    //     const showImage = () => {
-    //         console.log("Clicked!");
-    //     }
+<script setup>
+import { ref } from 'vue';
 
-    //     return showImage
-    // }
+let toggleImg = ref(false);
+
+function showImg() {
+    toggleImg.value = !toggleImg.value;
+    console.log(toggleImg.value);
+}
+
+function closeImg() {
+    toggleImg.value = false;
 }
 </script>
 <style></style>
