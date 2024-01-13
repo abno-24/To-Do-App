108ed5dd7ce5607182937b1b91c28c003b1962eb
Abno <abhinavmaurya605@gmail.com>
2024-01-14 01:02:49 +0530

    Worked on task list

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,43 @@
 <template>
-  <div id="app">
+  <div id="app" class="flex flex-col">
     <Header />
-    <ToDo />
     <Info />
-    <div v-for="task in tasks" :key="task">
-      <p>task</p>
+    <NewTask />
+    <hr class="my-4 bg-white" />
+    <div class="border-b-2 border-gray-200 max-md:w-[90%]">
+        <p class="italic font-extralight text-gray-400">Tasks</p>
     </div>
+    <ul class="max-md:w-[85%]">
+      <TaskList v-for="task in tasks" :key="task" :tasks="task" />
+    </ul>
   </div>
 </template>
-<script>
+<script setup>
 import Header from './components/Header.vue';
-import ToDo from './components/ToDo.vue';
+import NewTask from './components/NewTask.vue';
 import Info from './components/Info.vue';
+import TaskList from './components/TaskList.vue'
 
-export default {
-    setup() {
-      const tasks = [
-        {
-          id: 1,
-          name: "Gym",
-        },
-        {
-          id: 2,
-          name: "Book Reading",
-        },
-      ]
+const tasks = [
+  {
+    id: 1,
+    name: "Gym",
+  },
+  {
+    id: 2,
+    name: "Meditation",
+  },
+  {
+    id: 3,
+    name: "Journal Writing",
+  },
+  {
+    id: 4,
+    name: "Book Reading",
+  },
+]
 
-      return tasks;
-    },
-    components: { Header, ToDo, Info }
-}
+components: { Header, NewTask, Info, TaskList }
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,9 @@
+<template>
+    <li class="border-b-2 w-full py-2 pl-2 font-mono">
+        {{ tasks.id }}. {{ tasks.name }}
+    </li>
+</template>
+<script setup>
+defineProps(['tasks'])
+</script>
+<style></style>
